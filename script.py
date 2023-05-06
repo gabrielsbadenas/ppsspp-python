aa781859c954a6fb1a018b4a0e6660d42a77feee
gabriel <gabrielsbadenas@gmail.com>
2023-05-06 20:32:13 -0300

    render sprite

--- script.py
+++ script.py
@@ -1,18 +1,25 @@
 import psp2d
+
 font = psp2d.Font("font.png")
 image = psp2d.Image(480, 272)
 screen = psp2d.Screen()
+
 white = psp2d.Color(255,255,255)
 light = psp2d.Color(170,170,170)
 dark = psp2d.Color(86,86,86)
 black = psp2d.Color(0,0,0)
 CLEAR_COLOR =black 
+
 image.clear(CLEAR_COLOR)
 font.drawText(image, 0, 0, "hola mundo")
 font.drawText(image, 0, 30, "Press Circle to exit")
 image.putPixel(200,200,light)
 image.fillRect(0,0,16,16,dark)
+#actualiza la imagen
+sprite = psp2d.Image('gabissineun.png')
+screen.blit(sprite,480-16,272-16)
 screen.blit(image)
+screen.blit(sprite, 0, 0, 16,16, 400, 200, True)
 screen.swap()
 x = True
 while x == True:
