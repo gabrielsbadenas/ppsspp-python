110dd7b37906747a3896138f966320f360f1c816
gabriel <gabrielsbadenas@gmail.com>
2023-05-06 23:22:10 -0300

    mar

--- script.py
+++ script.py
@@ -1,14 +1,20 @@
 import psp2d
 from time import time,localtime,sleep
+import random
 start = time()
 Image = psp2d.Image
 Screen = psp2d.Screen
 Font = psp2d.Font
+Color = psp2d.Color
 
 font = Font("font.png")
 image = Image(480, 272)
 screen = Screen()
 swap = screen.swap
+
+size = 16
+horizontal = 480-size
+vertical = 272-size
 class Position:
     def __init__(self,x,y):
         self.x=x
@@ -29,6 +35,7 @@ white = psp2d.Color(255,255,255)
 light = psp2d.Color(170,170,170)
 dark = psp2d.Color(86,86,86)
 black = psp2d.Color(0,0,0)
+blue = psp2d.Color(164,219,232)
 CLEAR_COLOR =black 
 
 image.clear(dark)
@@ -48,13 +55,15 @@ def getTime():
 def timer():
     image.clear(dark)
     font.drawText(image, 0, 0, getTime())
+    font.drawText(image,random.randint(0,horizontal),random.randint(0,vertical),"'")
+    image.fillRect(0,vertical,480,16,psp2d.Color(164,219,232))#blue)
     screen.blit(image)
     render()
     swap()
 
 def render():
     #sprite = Image('gabissineun.png')
-    screen.blit(sprite, 0, 0, 16,16, int(getTime()), 272-16, True)
+    screen.blit(sprite, 0, 0, 16,16, int(getTime()), 272-32, True)
     #swap()
 
 def main():
